e24c43bd9077c19247fefde05854aa87e2c1806a
ErikRodriguez29 <115908225+ErikRodriguez29@users.noreply.github.com>
2026-05-18 19:46:10 -0700

    Recommender Implemented (#3)
    * Implemented data loading and category filtering in recommender system
    
    * Implemented the rest of filtering functions
    
    * Implemented pareto set optimization and recommender system
    
    * Rename output recommandation csvs to "recommendations.csv"
    
    * Improved ouput csv, adrecommendations, added weights to categorical variables and filtered recommendations by categorical_score
    
    * Switched to weights, removed references to paretoset
    
    * Implemented weights to scoring
    
    * Rounded scores for cleaner output
    
    * Added facility preferences to scoring
    
    * Formatted output of recommendations and saved them to a text file
    
    * Add a hard rain filter if the user prefers not go to while it rains
    
    * Add overall recommendations to output
    
    * Added an extra TODO
    
    * Changed categories, activites columns to be in alphabetic order
    
    * Merge branch 'database-dev' of https://github.com/ErikRodriguez29/Rec-Rec into erod-recommender-dev
    
    * Revert "Merge branch 'database-dev' of https://github.com/ErikRodriguez29/Rec-Rec into erod-recommender-dev"
    
    This reverts commit 51122ca6e4901ee7b81fe8dad7c7f7c18ae700ef.
    
    * Added extra TODOs, more details to TODOs
    
    * Added extra TODO
    
    * Added preferred facilities hard filter and error messages for filtering functions
    
    * Updated error messages
    
    * Implemented argument parsing from CLI
    
    * Implemented argument extraction (user input functions for CLI)
    
    * Removed TODO that's been done
    
    * Fixed overall recommendations
    
    * Fixed logic for formatting activity, category recommendations
    
    * Implemented recommendations json
    
    * Split recommender system into multiple files
    
    * Added weights to constants, updated help menu and docs

---
+++ scripts/recommender/constants.py
@@ -0,0 +1,100 @@
+from datetime import date, timedelta
+
+# Whether to use hard coded load paths for the current and next week numbers
+use_hard_coded_load_paths = False
+current_week_load_path = "../../predictions/Week 16/forecast_values.csv"
+next_week_load_path = "../../predictions/Week 17/forecast_values.csv"
+# Whether to use hard coded save paths for the current and next week numbers
+use_hard_coded_save_paths = False
+current_week_save_path = "../../predictions/Week 16/forecast_values_filtered.csv"
+next_week_save_path = "../../predictions/Week 17/forecast_values_filtered.csv"
+# Whether to use hard coded save paths for recommendation CSV output
+use_hard_coded_recommendations_save_paths = False
+current_week_recommendations_save_path = "../../predictions/Week 16/recommendations.csv"
+next_week_recommendations_save_path = "../../predictions/Week 17/recommendations.csv"
+
+# Weights to use for the optimization function
+percentage_filled_weight_constant = 0.33 # Weight for percentage filled
+preferred_day_hour_weight_constant = 20 # Weight for preferred day and hour
+raining_weight_constant = 5 # Weight for raining
+preferred_facility_weight_constant = 10 # Weight for preferred facility
+
+# List of available facilities
+available_facilities = [
+    "Racquetball Court 1",
+    "Racquetball Court 2",
+    "Racquetball Court 3",
+    "Racquetball Court 4",
+    "Squash Court 1",
+    "Galleria",
+    "Main Gym Court 1 (North)",
+    "Main Gym Court 2 (South)",
+    "Outdoor Fitness 1 (Turf, Free Weights, Benches)",
+    "Pavilion Court 1 (West)",
+    "Pavilion Court 2 (East)",
+    "Outdoor Fitness 2 (Behind Pottery)",
+    "FC 1- North Room",
+    "FC 1 - South Room",
+    "FC 2 - 1st floor",
+    "FC 2- Mezzanine",
+    "FC 3 - MAC",
+    "MAC Court",
+    "Spa",
+    "Small Pool",
+    "Big Pool",
+    "Pool Deck",
+    "Climbing Center - MAC",
+]
+
+# List of available activities
+available_activities = [
+    "racquetball",
+    "squash",
+    "ellipticals (precor branded machines)",
+    "stairmasters (stair machines)",
+    "treadmills",
+    "basketball",
+    "benching",
+    "bike machines",
+    "weight lifting",
+    "badminton",
+    "arm machines",
+    "core machines",
+    "leg presses",
+    "arm & leg machines",
+    "stairmasters",
+    "weight crunch machines",
+    "hockey",
+    "skating",
+    "swimming",
+    "climbing",
+    # Some facilities ("Spa", "Pool Deck") have activities listed as "NA" which are excluded from the available activities
+]
+
+# List of available exercise categories
+available_exercise_categories = [
+    "cardio",
+    "arms",
+    "core",
+    "legs",
+    "weight training",
+    # Some facilities ("MAC Court", "Pool Deck", "Spa", "Climbing Center - MAC") use "NA" as a category which are excluded from the available exercise categories
+]
+
+# Weeks start Monday; January 16, 2026 is in calendar week whose Monday is 2026-01-12.
+starting_date = date(2026, 1, 16)
+anchor_monday = starting_date - timedelta(days=starting_date.weekday())
+
+# Convert the day of the week to a name
+day_to_name = {
+    "M": "Monday",
+    "T": "Tuesday",
+    "W": "Wednesday",
+    "R": "Thursday",
+    "F": "Friday",
+    "S": "Saturday",
+    "U": "Sunday",
+}
+
+# Order of the days of the week
+day_order = ["M", "T", "W", "R", "F", "S", "U"]
